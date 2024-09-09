ae6d6f79b9d3c578263c712a61f861d9db99ea43
Himmath kumar <himmathchoudhary56@gmail.com>
2024-09-09 22:49:52 +0530

    Add .gitattributes to enforce LF line endings

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const  username ="chai aur code"
 
   return (
   <div>
     <Chai/>
-    <h1>Chai aur React</h1>
+    {<h1>Chai aur React {username}</h1>    /* {username} evaluated expression */}
     <p>Test para</p>
   </div>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,44 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// import {jsx as _jsx} from "react/jsx-runtime.js"
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom  App !</h1>
+        </div>
+    )
+}
+
+
+// const ReactElement ={
+//     type:'a',
+//     props:{
+//         href:"https://google.com",
+//         target:"_blank",                                             
+//     },
+//     children:'click me to visit google',
+// }
+
+const anotherElement =(
+    <a href="htpps://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser ="Chai aur React"
+
+const reactElement =React.createElement(
+    'a',
+    {
+        href:"https://google.com",
+        target:"_blank",
+    },
+    'click me to visit google',
+    anotherUser         /* evaluated expression*/ 
+)
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  reactElement
+    // <App/>
  
 )

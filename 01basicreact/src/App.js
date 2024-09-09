649836d357d118f38d70f3a1d578caa5e6153912
Himmath kumar <himmathchoudhary56@gmail.com>
2024-09-09 20:00:31 +0530

    configured react and vite app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello world |Himmath</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1>Hello world ,React with vite| Himmath Kumar</h1>
+  )
+}
+
+export default App
